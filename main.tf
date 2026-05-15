c6d24a7398b19c3c5d8832b5c0e3a8cb0e19f144
Ganeshraja <108399620+Ganeshraja1992@users.noreply.github.com>
2026-05-15 10:34:11 +0530

    Add tags to AppServerInstance configuration

--- main.tf
+++ main.tf
@@ -66,6 +66,7 @@ resource "aws_instance" "app_server" {
   instance_type          = "t3.micro"
   subnet_id              = module.subnets.public_subnet_id[0]
   vpc_security_group_ids = [aws_security_group.app_server.id]
+}
 
   tags = {
     Name = "AppServerInstance"
