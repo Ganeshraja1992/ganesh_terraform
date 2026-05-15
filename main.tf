b0a4435b0f12303e1739981a4651ca6b069e35c7
Ganeshraja <108399620+Ganeshraja1992@users.noreply.github.com>
2026-05-15 10:37:11 +0530

    Fix formatting in main.tf

--- main.tf
+++ main.tf
@@ -66,7 +66,6 @@ resource "aws_instance" "app_server" {
   instance_type          = "t3.micro"
   subnet_id              = module.subnets.public_subnet_id[0]
   vpc_security_group_ids = [aws_security_group.app_server.id]
-}
 
   tags = {
     Name = "AppServerInstance"
